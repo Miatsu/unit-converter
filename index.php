ea9b6d8d7c989da86a566cb2581d7ff9fce1b014
Caroline Fels <caroline.fels@gmail.com>
2018-03-03 01:11:25 +0100

    Change template file to use Twig

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 
+require_once 'vendor/autoload.php';
+
+$loader = new Twig_Loader_Filesystem('templates');
+$twig = new Twig_Environment($loader);
+
 function pre(array $array) {
     echo '<pre>';
     print_r($array);
@@ -13,17 +18,11 @@ function convert_volume($volume_to_convert, $unit_to_convert, $unit_converted, a
 
 pre($_GET);
 
-// $current_volume_to_convert = null;
-// if (isset($_GET['volume_to_convert'])) {
-//     $current_volume_to_convert = $_GET['volume_to_convert'];
-// }
-
 $current_volume_to_convert = (int) (isset($_GET['volume_to_convert']) ? $_GET['volume_to_convert'] : null);
 $current_volume_unit_to_convert = isset($_GET['volume_unit_to_convert']) ? $_GET['volume_unit_to_convert'] : null;
 $current_volume_unit_converted = isset($_GET['volume_unit_converted']) ? $_GET['volume_unit_converted'] : null;
 
 $volume_units = ['ml' => 0.001, 'cl' => 0.01, 'dl' => 0.1, 'l' => 1];
-
 
 if ($current_volume_to_convert !== null && $current_volume_unit_to_convert !== null && $current_volume_unit_converted !== null) {
     $volume_converted = convert_volume(
@@ -36,8 +35,10 @@ if ($current_volume_to_convert !== null && $current_volume_unit_to_convert !== n
     $volume_converted = 0;
 }
 
-
-
-
-
-require 'templates/index.template.php';
+echo $twig->render('index.twig', [
+    'current_volume_to_convert' => $current_volume_to_convert,
+    'current_volume_unit_to_convert' => $current_volume_unit_to_convert,
+    'current_volume_unit_converted' => $current_volume_unit_converted,
+    'volume_units' => $volume_units,
+    'volume_converted' => $volume_converted,
+]);
